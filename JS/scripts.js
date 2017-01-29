1d1c0fcde0d51ac296e374148ccd52e616be89b6
JDjedi <JDjedi1201@gmail.com>
2017-01-28 23:40:08 -0600

    Cleared up some bugs in multiple operators being passed in

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,129 +3,86 @@
 var inputArray = [];
 var displayInput = [];
 
-function add(arg, arg1) {
-  var answer = Number(arg) + Number(arg1);
-  document.getElementById('screen-output').innerHTML = answer;
-}
-
-function subtract(arg, arg1) {
-  var answer = Number(arg) - Number(arg1);
-  document.getElementById('screen-output').innerHTML = answer;
-}
-
-function multiply(arg, arg1) {
-  var answer = Number(arg) * Number(arg1);
-  document.getElementById('screen-output').innerHTML = answer;
-}
-
-function divide(arg, arg1) {
-  var answer = Number(arg) / Number(arg1);
-  document.getElementById('screen-output').innerHTML = answer;
-}
-
 function userSelect1() {
 	var input = document.getElementById('1').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect2() {
 	var input = document.getElementById('2').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect3() {
 	var input = document.getElementById('3').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect4() {
 	var input = document.getElementById('4').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect5() {
 	var input = document.getElementById('5').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect6() {
 	var input = document.getElementById('6').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect7() {
 	var input = document.getElementById('7').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect8() {
 	var input = document.getElementById('8').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect9() {
 	var input = document.getElementById('9').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelect0() {
 	var input = document.getElementById('0').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelectMultiply() {
 	var input = document.getElementById('*').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelectSubtract() {
 	var input = document.getElementById('-').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelectAdd() {
 	var input = document.getElementById('+').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelectDivide() {
 	var input = document.getElementById('/').innerHTML;
-	inputArray.push(input);
-	displayInput.push(input);
-	document.getElementById('screen-output').innerHTML = displayInput.join('');
+	userInput(input);
 }
 
 function userSelectClear() {
 	inputArray = [];
 	displayInput = [];
 	document.getElementById('screen-output').innerHTML = displayInput;
-	console.log(inputArray);
+}
+
+function userInput(input) {
+	inputArray.push(input);
+	displayInput.push(input);
+	document.getElementById('screen-output').innerHTML = displayInput.join('');
 }
 
 function userSelectEqual() {
@@ -147,23 +104,49 @@ function userSelectEqual() {
 
 function arithmaticMaker (arg) {
 	arg = arg.join('');
-	if(arg.match(/[+]/)) {
-		arg = arg.split('+');
-		add(arg[0], arg[1]);
-	} else if(arg.match(/[-]/)){
-		arg = arg.split('-');
-		subtract(arg[0], arg[1]);
-	} else if(arg.match(/[/]/)){
-		arg = arg.split('/');
-		divide(arg[0], arg[1]); 
-	} else if(arg.match(/[*]/)){
-		arg = arg.split('*');
-		multiply(arg[0], arg[1]);
+
+	if(arg.match(/[+-/*]{2,}/)) {
+		arg = [];
+		var errorMessage = "Error";
+		document.getElementById('screen-output').innerHTML = errorMessage;
 	} else {
-		console.log("Error");
+		if(arg.match(/[+]/)) {
+			arg = arg.split('+');
+			add(arg[0], arg[1]);
+		} else if(arg.match(/[-]/)){
+			arg = arg.split('-');
+			subtract(arg[0], arg[1]);
+		} else if(arg.match(/[/]/)){
+			arg = arg.split('/');
+			divide(arg[0], arg[1]); 
+		} else if(arg.match(/[*]/)){
+			arg = arg.split('*');
+			multiply(arg[0], arg[1]);
+		} else {
+			console.log("Error");
+		}
 	}
 }
 
+function add(arg, arg1) {
+  var answer = Number(arg) + Number(arg1);
+  document.getElementById('screen-output').innerHTML = answer;
+}
+
+function subtract(arg, arg1) {
+  var answer = Number(arg) - Number(arg1);
+  document.getElementById('screen-output').innerHTML = answer;
+}
+
+function multiply(arg, arg1) {
+  var answer = Number(arg) * Number(arg1);
+  document.getElementById('screen-output').innerHTML = answer;
+}
+
+function divide(arg, arg1) {
+  var answer = Number(arg) / Number(arg1);
+  document.getElementById('screen-output').innerHTML = answer;
+}
 
 
 
